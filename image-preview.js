82612361c5df76e90ca4622d2413ef1660696033
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2022-04-05 03:01:31 -0400

    add Projects page

---
+++ image-preview.js
@@ -0,0 +1,49 @@
+const clearChildren = (el) => {
+    while(el.firstChild) {
+        el.removeChild(el.firstChild);
+    }
+};
+
+window.addEventListener("load", function () {
+    // create the popup dynamically
+    const popupBackground = document.createElement("div");
+    const popup = document.createElement("div");
+    const popupBar = document.createElement("div");
+    const popupContent = document.createElement("div");
+    
+    popupBackground.id = "popup-background";
+    popupBackground.classList.add("hidden");
+    popup.id = "popup";
+    popupBar.id = "popup-bar";
+    popupBar.textContent = "x";
+    popupContent.id = "popup-content";
+    
+    popup.appendChild(popupBar);
+    popup.appendChild(popupContent);
+    popupBackground.appendChild(popup);
+    document.body.insertBefore(popupBackground, document.body.firstChild);
+    
+    const hidePopup = () => {
+        popupBackground.classList.add("hidden");
+    };
+    
+    popupBar.addEventListener("click", hidePopup);
+    
+    popupBackground.addEventListener("click", function (ev) {
+        if(ev.target === popupBackground) {
+            hidePopup();
+        }
+    });
+    
+    for(let img of document.querySelectorAll("img.preview")) {
+        img.addEventListener("click", function () {
+            popupBackground.classList.remove("hidden");
+            // create content
+            clearChildren(popupContent);
+            let largeImage = document.createElement("img");
+            largeImage.src = img.src;
+            largeImage.classList.add("display");
+            popupContent.appendChild(largeImage);
+        });
+    }
+});
